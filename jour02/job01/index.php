3c0fbf3e936fb562267852dcce59e440372b8b66
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-03 11:19:35 +0100

    job1 success

---
+++ jour02/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$int = 0;
+
+for ($int = 0; $int <= 1337; $int++ ) {
+    
+    if ($int == 42 ) {
+        echo "<b><u> $int </b></u>" . "</br>";
+    }
+    else {
+        echo $int . "</br>";
+    }
+   
+}
