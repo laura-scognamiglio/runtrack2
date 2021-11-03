cf983d98d503434203b311e077d01754e9e2d6f3
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-03 15:25:02 +0100

    job03 success

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -7,7 +7,12 @@ for ($int = 0; $int <= 100; $int++ ) {
     if ( $int >= 0 && $int <= 20 ) {
         echo  "<i>" . $int . "<br></i>";
     }
-
+    elseif ($int == 42 ){
+        echo  "<u>" . "La PLateforme_" . "<br></u>";
+    }
+    elseif ($int > 25 && $int < 50 ){
+        echo  "<u>" . $int . "<br></u>";
+    }
     else {
         echo $int . "</br>";
     }
