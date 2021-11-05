146f0c5384b4a31acc94c79210ac09160cbd58c7
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-05 17:19:25 +0100

    job02 jour4

---
+++ jour04/job02/index.php
@@ -0,0 +1,37 @@
+<form action="index.php" method="GET"> 
+    <label for="nom">Nom : </label>
+    <input type="text" name="nom"><br>
+    <label for="prenom">Prénom : </label>
+    <input type="text" name="prenom"><br>
+    <input type="submit" value="valider">
+</form>
+
+<table>
+    <thead>
+        <tr>
+            <th>Arguments</th>
+            <th>Valeur</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+        <?php
+        //isset permet de determiner si ce qu'il y a entre parentheses est determiné et non null
+        // if (isset($_GET['nom']) && isset($_GET['prenom']))
+        {
+            foreach($_GET as $key => $value){
+                echo "
+                <tr>
+                    <td>$key</td>
+                    <td>$value</td>
+                </tr>
+                ";
+            }
+        }
+        
+        ?>
+        
+    </tbody>
+</table>
+
+
