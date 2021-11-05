48a0fc78c184d0837ff90a1d62a43e821ed22e1e
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-05 14:30:22 +0100

    job01 jour4

---
+++ jour04/job01/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// $_GET['nom'];
+// $_GET['prenom'];
+
+$var = array(
+    'nom' => $_GET['nom'],
+    'prenom' => $_GET['prenom']
+);
+
+
+
+?>
+
+<form action="index.php" method="GET"> 
+    <label for="nom">Nom : </label>
+    <input type="text" name="nom"><br>
+    <label for="prenom">Prénom : </label>
+    <input type="text" name="prenom"><br>
+    <input type="submit" value="valider">
+</form>
+
+<?php
+// var_dump($_GET['prenom']);
+// var_dump($_GET['nom']);
+
+foreach($_GET as $key => $value){
+    echo $key . "<br>";
+    echo $value . "<br>";
+}
