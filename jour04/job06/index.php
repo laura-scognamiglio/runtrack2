63aee529c8e22c4b7053975d1b2a3c49729070bb
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-07 13:47:07 +0100

    job06 jour 4

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,2 +1,23 @@
+
+
+<form action="index.php" method="POST"> 
+    <label for="valeur">Nombre : </label>
+    <input type="number" name="nbr"><br>
+    <input type="submit" value="valider">
+</form>
+
 <?php
 
+$int = 0;
+
+if (isset($_POST['nbr']) == true){
+
+    foreach($_POST as $key => $value){
+        
+            if ($_POST['nbr'] %2 == 0)
+            echo "Nombre paire" ;
+            else {
+                echo "Nombre impair";
+            }
+    }
+}
