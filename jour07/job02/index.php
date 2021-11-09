03f9f3dddd6e35472fd3a6d8d5b7fa501bb5486b
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-09 12:08:39 +0100

    job2 jour7

---
+++ jour07/job02/index.php
@@ -0,0 +1,42 @@
+<!-- 
+if(!isset($_COOKIE['nbvisites']))
+{
+    setcookie('nbvisites',1,time()+30*24*3600);
+}
+else
+{
+    setcookie('nbvisites',$_COOKIE['nbvisites'] + 1,time()+30*24*3600);
+    echo $_COOKIE['nbvisites'];
+
+}
+
+var_dump($_COOKIE);
+if(isset($_POST["reset"])==true){
+    setcookie('nbvisites');
+}
+
+
+
+
+<form action= "index.php" method= "POST"> 
+    <input name= "reset" type= "submit" value= "Reset">
+</form> -->
+
+
+<?php
+
+if(!isset($_COOKIE['nbvisites'])){
+    setcookie('nbvisites',1,time()+ 3600);
+}
+else{
+    setcookie('nbvisites',$_COOKIE['nbvisites']+1,time()+ 3600);
+    echo $_COOKIE['nbvisites'];
+}
+if(isset($_POST['reset'])){
+    setcookie('nbvisites');
+}
+
+?>
+<form action= "index.php" method= "POST"> 
+    <input name= "reset" type= "submit" value= "Reset">
+</form> 
