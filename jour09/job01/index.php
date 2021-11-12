1097b751d089a8a4c0149803909a959451511c69
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-12 11:11:50 +0100

    job01 jour9

---
+++ jour09/job01/index.php
@@ -0,0 +1,37 @@
+<?php
+$bdd = mysqli_connect('localhost','root','root','jour08');
+$requete = mysqli_query($bdd, "SELECT * FROM etudiants");
+// $prenom = mysqli_query($bdd, "SELECT `prenom` FROM etudiants");
+$student = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+mysqli_set_charset($bdd, 'utf8');
+echo '<pre>';
+var_dump($student);
+echo '</pre>';
+?>
+
+<table>
+
+    
+    <thead>
+        <tr>
+            <td>prenom</td>
+            <td>nom</td>
+            <td>naissance</td>
+            <td>sexe</td>
+            <td>email</td>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach($student as $students): ?>
+        <tr>
+            <td><?=$students['prenom'];?></td>
+            <td><?=$students['nom'];?></td>
+            <td><?=$students['naissance'];?></td>
+            <td><?=$students['sexe'];?></td>
+            <td><?=$students['email'];?></td>
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
+
+
