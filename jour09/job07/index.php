898e98788653955fcf7445fdbf5d767e87847529
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-12 15:29:29 +0100

    job7 rectif

--- jour09/job07/index.php
+++ jour09/job07/index.php
@@ -1,31 +1,42 @@
-
 <?php
 $bdd = mysqli_connect('localhost','root','root','jour08');
-$requete = mysqli_query($bdd, "SELECT SUM(`superficie`) FROM `etage`");
-$salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
-mysqli_set_charset($bdd, 'utf8');
-echo '<pre>';
-var_dump($salles);
-echo '</pre>';
+mysqli_set_charset($bdd, "utf8");
+$requete = mysqli_query($bdd, "SELECT * FROM etage");
+$etages = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+$requete2 = mysqli_query($bdd, "SELECT SUM(superficie) FROM etage");
+$etageSpecify = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
 ?>
-
 <table>
-
-    
     <thead>
         <tr>
-            <td>superficie_totale</td>
-            
+            <th>id</th>
+            <th>nom</th>
+            <th>numero</th>
+            <th>superficie</th>
         </tr>
     </thead>
     <tbody>
-    <?php foreach($salles as $salle): ?>
-        <tr>
-            <td><?=$salle["SUM(`superficie`)"];?></td>
-            
-        </tr>
+    <?php foreach($etages as $etage): ?>
+            <tr>
+                <td><?=$etage['id'];?></td>
+                <td><?=$etage['nom'];?></td>
+                <td><?=$etage['numero'];?></td>
+                <td><?=$etage['superficie'];?></td>
+            </tr>
     <?php endforeach;?>
     </tbody>
 </table>
 
+<table>
+    <thead>
+        <tr>
+            <th>superficie_totale</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($etageSpecify as $specify): ?>
+            <td><?=$specify["SUM(superficie)"];?></td>
+        <?php endforeach;?>
+    </tbody>
+</table>
 
