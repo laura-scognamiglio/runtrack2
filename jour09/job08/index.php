809c940f29c81fdd2ff06c422709a3479ca48042
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-12 15:50:44 +0100

    job8 rectif

--- jour09/job08/index.php
+++ jour09/job08/index.php
@@ -1,28 +1,48 @@
 
 <?php
 $bdd = mysqli_connect('localhost','root','root','jour08');
-$requete = mysqli_query($bdd, "SELECT SUM(`capacites`) FROM salles");
-$salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+$requete = mysqli_query($bdd, "SELECT * FROM salles");
+$salle = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+$requete2 = mysqli_query($bdd, "SELECT SUM(`capacites`) FROM salles");
+$salleSpecify = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
 mysqli_set_charset($bdd, 'utf8');
 echo '<pre>';
-var_dump($salles);
+var_dump($salleSpecify);
 echo '</pre>';
 ?>
+<table>
+<thead>
+    <tr>
+        <td>id</td>
+        <td>nom</td>
+        <td>id_etage</td>
+        <td>capacites</td>
+        
+    </tr>
+</thead>
+<tbody>
+<?php foreach($salle as $salles): ?>
+    <tr>
+        <td><?=$salles["id"];?></td>
+        <td><?=$salles["nom"];?></td>
+        <td><?=$salles["id_etage"];?></td>
+        <td><?=$salles["capacites"];?></td>
+        
+    </tr>
+<?php endforeach;?>
+</tbody>
+</table>
 
 <table>
-
-    
     <thead>
         <tr>
             <td>superficie_totale</td>
-            
         </tr>
     </thead>
     <tbody>
-    <?php foreach($salles as $salle): ?>
+    <?php foreach($salleSpecify as $salle): ?>
         <tr>
             <td><?=$salle["SUM(`capacites`)"];?></td>
-            
         </tr>
     <?php endforeach;?>
     </tbody>
