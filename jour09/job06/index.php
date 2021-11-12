187433f7aed59c4c6690e9a815bcef425c80cfed
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-12 15:07:24 +0100

    job6 rectif

--- jour09/job06/index.php
+++ jour09/job06/index.php
@@ -1,11 +1,13 @@
 
 <?php
 $bdd = mysqli_connect('localhost','root','root','jour08');
-$requete = mysqli_query($bdd, "SELECT COUNT(*) FROM `etudiants`");
+$requete = mysqli_query($bdd, "SELECT * FROM `etudiants`");
 $student = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+$requete2 = mysqli_query($bdd, "SELECT COUNT(*) FROM `etudiants`");
+$studentSpecify = mysqli_fetch_all($requete2, MYSQLI_ASSOC);
 mysqli_set_charset($bdd, 'utf8');
 echo '<pre>';
-var_dump($student);
+var_dump($studentall);
 echo '</pre>';
 ?>
 
@@ -14,18 +16,49 @@ echo '</pre>';
     
     <thead>
         <tr>
+            <td>id</td>
+            <td>prenom</td>
+            <td>nom</td>
+            <td>naissance</td>
+            <td>sexe</td>
+            <td>email</td>
+            
+            
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($student as $students): ?>
+                <tr>
+                    <td><?=$students['id'];?></td>
+                    <td><?=$students['prenom'];?></td>
+                    <td><?=$students['nom'];?></td>
+                    <td><?=$students['naissance'];?></td>
+                    <td><?=$students['sexe'];?></td>
+                    <td><?=$students['email'];?></td>
+                    
+                    
+                </tr>
+        <?php endforeach;?>
+    </tbody>
+</table>
+<table>
+
+    
+    <thead>
+        <tr>
+            
             <td>nb_etudiants</td>
             
         </tr>
     </thead>
     <tbody>
-    <?php foreach($student as $students): ?>
-        <tr>
-            <td><?=$students["COUNT(*)"];?></td>
-            
-        </tr>
-    <?php endforeach;?>
+            <?php foreach($studentSpecify as $specify): ?>
+                <tr>
+                    
+                    <td><?=$specify["COUNT(*)"];?></td>
+                    
+                </tr>
+        <?php endforeach;?>
     </tbody>
 </table>
-
 
