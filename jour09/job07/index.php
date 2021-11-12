c7a23fdfc3c5d8718f87c36597fac3e978b5f32d
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2021-11-12 13:40:37 +0100

    job07 jour9

---
+++ jour09/job07/index.php
@@ -0,0 +1,31 @@
+
+<?php
+$bdd = mysqli_connect('localhost','root','root','jour08');
+$requete = mysqli_query($bdd, "SELECT SUM(`superficie`) FROM `etage`");
+$salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+mysqli_set_charset($bdd, 'utf8');
+echo '<pre>';
+var_dump($salles);
+echo '</pre>';
+?>
+
+<table>
+
+    
+    <thead>
+        <tr>
+            <td>superficie_totale</td>
+            
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach($salles as $salle): ?>
+        <tr>
+            <td><?=$salle["SUM(`superficie`)"];?></td>
+            
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
+
+
